595afb64ee79f8e054eee96d6ff25c9ac2786c25
Kenneth Quintero <kennethgqv@gmail.com>
2022-01-01 02:12:39 -0700

    Pre solution

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import Optional
 from fastapi import FastAPI
+from typing import Optional
 from model.dbHandler import match_exact, match_like
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 @app.get("/")
@@ -12,11 +12,12 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    return "TODO"
+    response = {'usage': '/dict?word=<word>'}
+    return response
 
 
 @app.get("/dict")
-def dictionary():
+def dictionary(word: str):
     """
     DEFAULT ROUTE
     This method will
@@ -24,4 +25,19 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+    if not word:
+        response = {'status': 'error', 'word': word, 'data': 'word not found'}
+        return response
+
+    definitions = match_exact(word)
+    if definitions:
+        response = {'status': 'success', 'word': word, 'data': definitions}
+        return response
+
+    definitions = match_like(word)
+    if definitions:
+        response = {'status': 'success', 'word': word, 'data': definitions}
+        return response
+    else:
+        response = {'status': 'error', 'word': word, 'data': 'word not found'}
+        return response
